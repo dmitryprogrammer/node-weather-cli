c9a82c39a24361fd484edaa6b8fe6b20a37c848d
dmitryprogrammer <dpischalka@gmail.com>
2024-12-19 11:29:27 +0300

    init-first-commands-functional: try commander.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log(process.arch);
+import { program } from "commander";
+
+program.option("--first").option("-s, --separator <char>");
+
+program.parse();
+
+console.log(program.args);
+
+console.log(process.argv);
