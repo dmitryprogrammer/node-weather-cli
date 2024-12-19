4de30888c1ebe65f07a2509951f3c0a39625d25a
dmitryprogrammer <dpischalka@gmail.com>
2024-12-19 13:10:11 +0300

    build(create-basement-for-task-interaction): created prototype of connector for read/write tasks from db.

---
+++ src/db/connector.ts
@@ -0,0 +1,33 @@
+import { readFile, writeFile } from "fs/promises";
+import { join } from "path";
+
+export type Task = {
+  id: number;
+  title: string;
+};
+
+export class Connector {
+  private readonly dbPath = join(__dirname, "./tasks.db.json");
+
+  async getTaskToDb(taskTitle: Task["title"]) {
+    try {
+      const task = await readFile(this.dbPath, "utf-8");
+      console.log(task);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async writeTaskToDb(task: Task) {
+    try {
+      const result = await writeFile(
+        this.dbPath,
+        JSON.stringify(task),
+        "utf-8"
+      );
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
